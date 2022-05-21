40e5c81e6c6352760a87ce9b7dedb8876e906c78
Adrian Bustos <bustosadrian@gmail.com>
2022-05-21 14:27:59 +0200

    Grading Students - Fixing wrong sample

--- GradingStudents/Program.cs
+++ GradingStudents/Program.cs
@@ -3,33 +3,19 @@ using System.Collections.Generic;
 
 namespace GradingStudents
 {
-    class Program
+    class Result
     {
         private const int FAILING_GRADE = 40;
         private const int GRADE_INTERVAL = 5;
         private const int GRADE_INTERVAL_BREAK = 3;
 
-        static void Main(string[] args)
-        {
-            List<int> grades = new List<int> { 4, 73, 67, 38, 33 };
 
-
-            var result = GradingStudents(grades);
-
-            for(int i = 0; i < grades.Count; i++)
-            {
-                Console.WriteLine($"{grades[i]}: {result[i]}");
-            }
-
-            Console.ReadLine();
-        }
-
-        private static List<int> GradingStudents(List<int> grades)
+        public static List<int> gradingStudents(List<int> grades)
         {
             List<int> retval = null;
 
             retval = new List<int>();
-            for(int i = 0; i < grades.Count; i++)
+            for (int i = 0; i < grades.Count; i++)
             {
                 retval.Add(GradeStudent(grades[i]));
             }
@@ -43,12 +29,40 @@ namespace GradingStudents
 
             int interval_diff = GRADE_INTERVAL - grade % GRADE_INTERVAL;
 
-            if(interval_diff < GRADE_INTERVAL_BREAK)
+            if (interval_diff < GRADE_INTERVAL_BREAK)
             {
                 roundedGrade = roundedGrade + interval_diff;
             }
 
             return roundedGrade < FAILING_GRADE ? grade : roundedGrade;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> grades = new List<int> { 4, 73, 67, 38, 33 };
+
+            var result = Result.gradingStudents(grades);
+
+            Console.WriteLine("Grades");
+            Console.WriteLine("**************************");
+            foreach (int o in grades)
+            {
+                Console.WriteLine($"{o}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Result");
+            Console.WriteLine("**************************");
+            foreach (int o in result)
+            {
+                Console.WriteLine($"{o}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
